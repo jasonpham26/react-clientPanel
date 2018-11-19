4069c03d989b9647b0721506d026e9936ebf3fd0
jasonpham26 <vinhphuong0206@gmail.com>
2018-11-19 20:53:11 +1000

    Client details

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-class Clients extends Component {
-  render() {
-    const clients = [
-      {
-        id: '43333444',
-        firstName: 'Kevin',
-        lastName: 'Pham',
-        email: 'kevin@gmail.com',
-        phone: '555-555-555',
-        balance: '30'
-      },
-      {
-        id: '333333222',
-        firstName: 'Kevin',
-        lastName: 'Jensen',
-        email: 'jensen@gmail.com',
-        phone: '444-333-555',
-        balance: '50.43'
-      }
-    ];
+import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import Spinner from '../layout/Spinner';
 
+class Clients extends Component {
+  state = {
+    totalOwed: null
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    const { clients } = props;
+
+    if (clients) {
+      // Add balances
+      const total = clients.reduce((total, client) => {
+        return total + parseFloat(client.balance.toString());
+      }, 0);
+
+      return { totalOwed: total };
+    }
+
+    return null;
+  }
+
+  render() {
+    const { clients } = this.props;
+    const { totalOwed } = this.state;
     if (clients) {
       return (
         <div>
@@ -30,6 +38,14 @@ class Clients extends Component {
                 {' '}
                 <i className="fas fa-users" /> Clients{' '}
               </h2>
+            </div>
+            <div className="col-md-6">
+              <h5 className="text-right text-secondary">
+                Total Owed {'  '}
+                <span className="text-primary">
+                  ${parseFloat(totalOwed).toFixed(2)}
+                </span>
+              </h5>
             </div>
           </div>
           <table className="table table-striped">
@@ -64,9 +80,19 @@ class Clients extends Component {
         </div>
       );
     } else {
-      return <h1 />;
+      return <Spinner />;
     }
   }
 }
 
-export default Clients;
+Clients.propTypes = {
+  firestore: PropTypes.object.isRequired,
+  clients: PropTypes.array
+};
+
+export default compose(
+  firestoreConnect([{ collection: 'clients' }]),
+  connect((state, props) => ({
+    clients: state.firestore.ordered.clients
+  }))
+)(Clients);
--- src/store.js
+++ src/store.js
@@ -26,3 +26,32 @@ const rrfConfig = {
 firebase.initializeApp(firebaseConfig);
 // Init firestore
 const firestore = firebase.firestore();
+const settings = { timestampsInSnapshots: true };
+firestore.settings(settings);
+
+// Add reactReduxFirebase enhancer when making store creator
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, rrfConfig), // firebase instance as first argument
+  reduxFirestore(firebase)
+)(createStore);
+
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer
+});
+
+// Create initial state
+const initialState = {};
+
+// Create store
+const store = createStoreWithFirebase(
+  rootReducer,
+  initialState,
+  compose(
+    reactReduxFirebase(firebase),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
